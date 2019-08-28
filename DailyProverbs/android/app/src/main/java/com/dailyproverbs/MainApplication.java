008e834773573659362f025bf87bbcf43dd39ba9
ChrisFlannagan <me@whoischris.com>
2019-08-28 09:34:11 -0400

    Building now with push notifications, but firebase auth is broken on launch

--- DailyProverbs/android/app/src/main/java/com/dailyproverbs/MainApplication.java
+++ DailyProverbs/android/app/src/main/java/com/dailyproverbs/MainApplication.java
@@ -3,6 +3,7 @@ package com.dailyproverbs;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNFirebasePackage(),
             new RNDeviceInfo(),
             new AsyncStoragePackage(),
